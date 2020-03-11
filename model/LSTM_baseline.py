94877ad83b3db7b6c74e23b0190add7f8eed05ab
zhangyuejoslin <949788345@qq.com>
2020-03-11 12:03:01 -0400

    data without blank line and baseline model

--- model/LSTM_baseline.py
+++ model/LSTM_baseline.py
@@ -1,5 +1,4 @@
 import torch
-from data_helper import data_reader
 
 def create_emb_layer(weights_matrix, non_trainable=False):
     emb_layer = torch.nn.Embedding.from_pretrained(torch.tensor(weights_matrix, dtype=torch.float), padding_idx=0)
@@ -7,15 +6,15 @@ def create_emb_layer(weights_matrix, non_trainable=False):
         emb_layer.weight.requires_grad = False
     return emb_layer
 
-class Model(torch.nn.Module):
+class LSTM_Model(torch.nn.Module):
     def __init__(self, weights_matrix,labels):
-        super(Model, self).__init__()
+        super(LSTM_Model, self).__init__()
         self.embedding = create_emb_layer(weights_matrix, False)
         self.rnn = torch.nn.LSTM(input_size=50, hidden_size=100, num_layers=2, batch_first=True, bidirectional=True)
-        self.linear = torch.nn.Linear(50, len(labels))
+        self.linear = torch.nn.Linear(200, len(labels))
 
     def forward(self,sentence):
-        token_idx = torch.tensor(data_reader.token_2_id(sentence))
-        rnn_embedding = self.rnn(self.embedding(token_idx))
+        emb = self.embedding(sentence).unsqueeze(1)
+        rnn_embedding, _ = self.rnn(emb)
         logits = self.linear(rnn_embedding)
         return logits
