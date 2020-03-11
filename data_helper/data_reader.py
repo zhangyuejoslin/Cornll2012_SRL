eaec9250c46b735e782f8a81f49c769ca37888a1
Drew Hayward <d.andrew.hayward@gmail.com>
2020-03-11 14:56:52 -0400

    Use torchtext vocabs in datareader (46x faster)

--- data_helper/data_reader.py
+++ data_helper/data_reader.py
@@ -1,13 +1,15 @@
+from collections import Counter
 import random
 import numpy as np
+from torchtext.vocab import Vocab
 '''
 sample example:
 5 We respectfully invite you to watch a special edition of Across China . ||| O O O B-ARG0 O B-V B-ARG1 I-ARG1 I-ARG1 I-ARG1 I-ARG1 I-ARG1 O
 where 5 is the index of predicate in this sample.
 '''
-def read_raw_sentence(file_path):
+def read_raw_sentences(file_path):
     word_label_pair_sentences = []
-    label_set = set()
+    label_counter = Counter()
     f = open(file_path, 'r')
     for line in f:
         line = line.strip().split('|||')
@@ -15,16 +17,16 @@ def read_raw_sentence(file_path):
         words = [word.lower() for word in words_tmp]
         labels = line[1].strip().split()
         for label in labels:
-            label_set.add(label)
+            label_counter[label] += 1
         if len(line) <= 1: ### if the sample only has words but not has labels
             labels = ['O' for i in words[1:]]
         predicate = int(words[0])
         word_label_pair_sentences.append([words[1:], labels, predicate]) ## x, y, index of predicate
     f.close()
-    return word_label_pair_sentences, list(label_set)
+    return word_label_pair_sentences, Vocab(label_counter)
 
-# ## test case for read_raw_sentence: pass
-# res, label_dict= read_raw_sentence('../data/BIO-formatted-sample/train.txt')
+# ## test case for read_raw_sentences: pass
+# res, label_dict= read_raw_sentences('../data/BIO-formatted-sample/train.txt')
 # print(res)
 # print(label_dict)
 
@@ -32,29 +34,23 @@ def read_raw_sentence(file_path):
 ### use glove embedding
 def load_embeddings(file_path):
     embeddings_res = []
-    vocab_list = []
-    vocab_list.append('UNK')
+    token_counter = Counter()
     f = open(file_path, 'r', encoding='UTF-8')
     for line in f:
         line = line.strip().split()
         embeddings_res.append([float(emb) for emb in line[1:]])
-        vocab_list.append(line[0])
+        token_counter[line[0]] += 1
     embeddings_res.insert(0, [random.gauss(0, 0.01) for _ in range(len(embeddings_res[0]))])
     f.close()
-    return embeddings_res, vocab_list
 
-### test case for read_raw_sentence: pass
-# embeddings_res, vocab_dict = load_embeddings('/home/hlr/shared/glove6B/glove.6B.50d.txt')
-# print(len(embeddings_res), len(vocab_dict))
+    return embeddings_res, Vocab(token_counter)
 
-def token_2_id(sequence, vocab_list):
-    ids = []
-    for word in sequence:
-        if word not in vocab_list:
-            ids.append(0)
-        else:
-            ids.append(vocab_list.index(word))
-    return ids
+### test case for read_raw_sentences: pass
+# embeddings_res, word_to_id = load_embeddings('/home/hlr/shared/glove6B/glove.6B.50d.txt')
+# print(len(embeddings_res), len(word_to_id))
+
+def sequence_to_id(sequence, vocab):
+    return [vocab.stoi[elem] for elem in sequence]
     
 # def label_2_id(sequence, label_dict):
 #     ids = []
@@ -64,22 +60,22 @@ def token_2_id(sequence, vocab_list):
 
 ### final outputs are numpy formats
 def data_preprocesing(train_file, dev_file, embed_file, max_len):
-    train_pair, label_set = read_raw_sentence(train_file)
-    # dev_pair, dev_label_set = read_raw_sentence(dev_file)
+    train_pair, label_vocab = read_raw_sentences(train_file)
+    # dev_pair, dev_label_set = read_raw_sentences(dev_file)
     # label_set = list(set(train_label_set + dev_label_set))
-    dev_pair, _ = read_raw_sentence(dev_file)
-    embed, vocab_list = load_embeddings(embed_file)
+    dev_pair, _ = read_raw_sentences(dev_file)
+    embed, token_vocab = load_embeddings(embed_file)
 
     # training data token to index
-    train_samples = [token_2_id(sent[0], vocab_list) for sent in train_pair]
+    train_samples = [sequence_to_id(sent[0], token_vocab) for sent in train_pair]
     train_mask = [len(sent[0]) for sent in train_pair]
-    train_labels = [token_2_id(sent[1], label_set) for sent in train_pair]
+    train_labels = [sequence_to_id(sent[1], label_vocab) for sent in train_pair]
     train_predicate = [sent[2] for sent in train_pair]
 
     # training data token to index
-    dev_samples = [token_2_id(sent[0], vocab_list) for sent in dev_pair]
+    dev_samples = [sequence_to_id(sent[0], token_vocab) for sent in dev_pair]
     dev_mask = [len(sent[0]) for sent in dev_pair]
-    dev_labels = [token_2_id(sent[1], label_set) for sent in dev_pair]
+    dev_labels = [sequence_to_id(sent[1], label_vocab) for sent in dev_pair]
     dev_predicate = [sent[2] for sent in train_pair]
 
     ### mask the training sample length
@@ -114,17 +110,21 @@ def data_preprocesing(train_file, dev_file, embed_file, max_len):
     dev_labels_np = np.array(dev_labels)
     dev_predicate_np = np.array(dev_predicate)
     emb_np = np.array(embed)
+
+    # Convert dicts back into lists for compatibility
   
     return (train_samples_np, train_mask_np, train_labels_np, train_predicate_np),\
-            (dev_samples_np, dev_mask_np, dev_labels_np, dev_predicate_np), emb_np, vocab_list, label_set
+            (dev_samples_np, dev_mask_np, dev_labels_np, dev_predicate_np), emb_np, token_vocab, label_vocab
 
 ### test data_preprocesing
-train, dev, emb, vocab, labels = data_preprocesing('../data/BIO-formatted-sample/train.txt',
-                                    '../data/BIO-formatted-sample/dev.txt',
-                                    '../data/glove.6B.50d.txt',20)
-# train, dev, emb, vocab, labels = data_preprocesing('../data/BIO-formatted/conll2012.train.txt',
-#                                     '../data/BIO-formatted/conll2012.devel.txt',
-#                                     '/home/hlr/shared/glove6B/glove.6B.50d.txt',20)
+#train, dev, emb, vocab, labels = data_preprocesing('../data/BIO-formatted-sample/train.txt',
+#                                    '../data/BIO-formatted-sample/dev.txt',
+#                                    '../data/glove.6B.50d.txt',20)
+
+if __name__ == "__main__":
+    train, dev, emb, tok_vocab, label_vocab = data_preprocesing('data/BIO-formatted/conll2012.train.txt',
+                                        'data/BIO-formatted/conll2012.devel.txt',
+                                        '/home/hlr/shared/data/glove6B/glove.6B.50d.txt',20)
 # print('first train sample data:', train[0][0])
 # print('first train sample mask:', train[1][0])
 # print('first train sample label:', train[2][0])
